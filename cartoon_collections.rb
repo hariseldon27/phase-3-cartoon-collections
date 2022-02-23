4bb7b328514099c46dab27438d43548c8f03493a
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-23 13:27:05 -0800

    commit tree 1553ee3f0496f1a35760602ca37e8f4ef7106ea2

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+def roll_call_dwarves(dwarf_array)
   # Your code here
+  dwarf_array.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map do |planeteer|
+    planeteer.capitalize() << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)# code an argument here
   # Your code here
+  words.any? { |word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(input_arry)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  shared = cheese_types & input_arry
+  if shared.length == 0
+    return nil
+  else
+    return shared[0]
+  end
 end
+binding.pry
+find_the_cheese(["crackers", "gouda", "thyme"])
